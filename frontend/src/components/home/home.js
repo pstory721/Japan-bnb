3d943e4af4109802a28e7131c776001240ac4e8b
Jacob <jstory0721@gmail.com>
2021-11-23 10:44:47 -0800

    jake

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import SignupFormModal from "../SignupFormModal";
 import "./home.css";
-
+//test/
 export function Home() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
